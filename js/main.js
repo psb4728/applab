cbe685b722fb19aedeb2a1d104320f0770e4b7f3
psb4728 <63276761+psb4728@users.noreply.github.com>
2020-10-22 16:54:27 +0900

    HTML, CSS, Javascript
    HTML, CSS, Javascript: html, css 수정 및 추가 Javascript 모바일 이벤트 추가

--- js/main.js
+++ js/main.js
@@ -13,6 +13,51 @@ document.addEventListener('scroll', function() {
 });
 
 
+// gnb menu 클릭시 원하는 섹션으로 이동
+var gnb = document.querySelector('.gnb');
+gnb.addEventListener('click', function(event) {
+  // 모바일 화면에서 nav-menu를 클릭시 active 클래스를 remove하기 위한 변수
+  var navWrap = document.querySelector('.nav');
+  var navbar = document.querySelector('.header');
+  
+  var target = event.target;
+  var link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  
+  navbar.classList.remove('active');
+  navWrap.classList.remove('active');
+  gnb.classList.remove('active');
+  scrollIntoView(link);
+});
+
+function scrollIntoView(selector) {
+  var scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
+
+// var gnb = document.querySelector('.header');
+// gnb.addEventListener('click', function(e) {
+//   // 모바일 화면에서 gnb를 클릭시 active 클래스를 reover하기 위한 변수
+//   var gnb = document.querySelector('gnb');
+//   var target = e.target;
+//   var link = target.dataset.link;
+//   if(link == null) {
+//     return;
+//   }
+
+//   gnb.classList.remove('active');
+//   gnb.classList.remove('active');
+//   scrollIntoView(link);
+// });
+
+// function scrollIntoView(selector) {
+//   var scrollTo = document.querySelector(selector);
+//   scrollTo.scrollIntoView({ behavior: 'smooth' });
+// }
+
+
 // toggle btn
 var menuIcon = document.querySelector('.toggle-btn');
 
@@ -22,3 +67,36 @@ menuIcon.addEventListener('click', function() {
   navWrap.classList.toggle('active');
   navbar.classList.toggle('active');
 });
+
+
+// back to top
+var btt = document.getElementById('back-to-top'),
+    docElem = document.documentElement,
+    offset,
+    scrollPos,
+    docHeight;
+
+docHeight = Math.max(docElem.offsetHeight, docElem.scrollHeight);
+  if(docHeight !== 0) {
+    offset = docHeight / 4;
+  } 
+
+  window.addEventListener('scroll', function() {
+    scrollPos = docElem.scrollTop;
+  
+    btt.className = (scrollPos > offset) ? 'visible' : '';
+  }); 
+
+btt.addEventListener('click', function(e) {
+  e.preventDefault();
+  scrollToTop();
+});
+function scrollToTop() {
+  var scrollInterval = setInterval(function() {
+    if(scrollPos !== 0) {
+      window.scrollBy(0, -150);
+    } else {
+      clearInterval(scrollInterval);
+    }
+  },15);
+}
