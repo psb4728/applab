c787028cb6c537ba14127953fa9ef2dc46ba0539
psb4728 <63276761+psb4728@users.noreply.github.com>
2021-01-11 22:42:24 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ var appIcons = document.querySelector('.app-icon .icon-wrap'),
     desc = document.querySelector('.icon-description'),
     summaryTit = document.querySelector('.smart-tit .summary-title'),
     summaryDesc = document.querySelector('.smart-tit .summary-sub-description'),
-    section = document.querySelector('.app-container').offsetTop - 400;
+    section = document.querySelector('.app-container').offsetTop - 350;
 
 function showScroll() {
   var currentScroll = window.pageYOffset;
